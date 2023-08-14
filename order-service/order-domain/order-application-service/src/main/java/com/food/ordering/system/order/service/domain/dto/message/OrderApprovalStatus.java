da227bacc021744ffd43c5403e3eac3de876f492
Ruslan <rus5i@>
2023-08-15 00:20:04 +0500

    Реализация уровня Application Service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/message/OrderApprovalStatus.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.order.service.domain.dto.message;
+
+public enum OrderApprovalStatus {
+
+    /**
+     * Одобрено
+     */
+    APPROVED,
+
+    /**
+     * Отклонено
+     */
+    REJECT;
+}
