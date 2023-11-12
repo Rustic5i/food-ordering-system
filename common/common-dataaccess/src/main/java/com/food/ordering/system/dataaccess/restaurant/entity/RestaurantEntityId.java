0aba067f9451a5a2b75b0cc1c77e98e1a62b6d29
Ruslan <rus5i@>
2023-11-12 19:57:36 +0500

    58 Реализация модуля restaurant-dataaccess

--- common/common-dataaccess/src/main/java/com/food/ordering/system/dataaccess/restaurant/entity/RestaurantEntityId.java
+++ common/common-dataaccess/src/main/java/com/food/ordering/system/dataaccess/restaurant/entity/RestaurantEntityId.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.dataaccess.restaurant.entity;
+package com.food.ordering.system.dataaccess.restaurant.entity;
 
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
